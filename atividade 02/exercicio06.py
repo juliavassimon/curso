85a9a59d00bc230b622f8f6cbfa659556da80fad
Júlia Vassimon <juliavassimonsilva@gmail.com>
2024-04-10 09:23:27 -0300

    Delete atividade 02 directory

--- atividade 02/exercicio06.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plot
-import numpy as np
-
-for row in range(2):
-    for col in range(2):
-
-
-ha='center', va='center', fontsize=18
--- atividade 02/exercicio08.py
+++
@@ -1,7 +0,0 @@
-#Utilizando pandas, como realizar a leitura de um arquivo CSV em um
-#DataFrame e exibir as primeiras linhas?
-
-import pandas as pd
-
-caminho_aquivo = 'caminho'
-dataframe = pd.read_csv(caminho_aquivo)
